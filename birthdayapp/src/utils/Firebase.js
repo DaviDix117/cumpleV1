9793b74b76b60ab92acd54566f5a44e2673c1812
David Galeano <davidalejangp@gmail.com>
2020-12-18 15:29:36 -0500

    Firebase yarn

---
+++ birthdayapp/src/utils/Firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyC_8yOFjnx55nmsxGNlWoI_PIth3w-XHGo",
+authDomain: "birthday-00117.firebaseapp.com",
+projectId: "birthday-00117",
+storageBucket: "birthday-00117.appspot.com",
+messagingSenderId: "182628003960",
+appId: "1:182628003960:web:3da87b23214c86ff44bd8d"
+};
